d74cabc066471372ba995053325b717e690ff23f
Samantha Olvera <smolvera2010@yahoo.com>
2018-11-15 19:55:31 -0500

    mock website with programmatic redirect example

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Contact = (props) => {
+    console.log(props);
+    //programatic redirect example
+    setTimeout(() => {
+        props.history.push("/about");
+    }, 2000);
+    return(
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Welcome to the Contact page</p>
+        </div>
+    )
+}
+
+export default Contact;

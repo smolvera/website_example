0bb336167a4014f28865a6de6cc2426dc3d32260
Samantha Olvera <smolvera2010@yahoo.com>
2018-11-15 19:53:07 -0500

    Updates

---
+++ precache-manifest.73bc81826ce5b042812cd016c189940f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c476e7dd43e37592c58",
+    "url": "/website_example/static/js/runtime~main.6c476e7d.js"
+  },
+  {
+    "revision": "cac7d5303a2bc5c98676",
+    "url": "/website_example/static/js/main.cac7d530.chunk.js"
+  },
+  {
+    "revision": "928f40b318a40df5521a",
+    "url": "/website_example/static/js/1.928f40b3.chunk.js"
+  },
+  {
+    "revision": "cac7d5303a2bc5c98676",
+    "url": "/website_example/static/css/main.8982af06.chunk.css"
+  },
+  {
+    "revision": "04881b765b5aa618cc8f55a09cdbcb17",
+    "url": "/website_example/index.html"
+  }
+];
